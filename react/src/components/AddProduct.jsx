405c4c3e3836b1c8178e85ea8569d564971e6451
Nedim89 <nedim.custovic@gmail.com>
2023-02-02 15:32:34 +0000

    Added CSS && PHP

--- react/src/components/AddProduct.jsx
+++ react/src/components/AddProduct.jsx
@@ -6,7 +6,7 @@ function AddProduct() {
   const [SKU,setSKU] = useState('');
   const [name,setName] = useState('');
   const [price,setPrice] = useState('');
-  const [typeSwitch,setTypeSwitch] = useState(false);
+  const [typeSwitch,setTypeSwitch] = useState("DVD");
   const [size,setSize] = useState('');
   const [height,setHeight] = useState('');
   const [width,setWidth] = useState('');
@@ -53,51 +53,79 @@ function AddProduct() {
 
   return (
     <>
-      <div>
+    <div className="wrap-header">
       <h1>Product Add</h1>
-      <div>
-        <button>Save</button>
-        <button>Cancel</button>
+      <nav>
+      <a className="link_button" to="addproduct" onClick={()=>{console.log("Submited")}}> Save </a>
+      <a className="link_button">Cancel</a>
+      </nav>
       </div>
-      </div>
+    
+      <div className="line"></div>
       
-      <div className='container'>
-        <label htmlFor="SKU">SKU</label>
-        <input type="text" name="SKU" id="SKU" value={SKU} onChange={(e) => setSKU(e.target.value)} />
 
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
 
-        <label htmlFor="price">Price</label>
-        <input type="number" name="price" id="price" value={price} onChange={(e) => setPrice(e.target.value)} />
 
-        <label htmlFor="typeSwitch">Type Switcher</label>
-        <input type="checkbox" name="typeSwitch" id="typeSwitch" value={typeSwitch} onChange={(e) => setTypeSwitch(e.target.checked)} />
+      
+      <form
+                id="product_form"
+                action="http://localhost:8000/server.php"
+                method="post"
+                onSubmit={(event) => handleSumbit(event)}
+            >
+        <div>
+        <span>SKU</span>
+        <span><input type="text" name="SKU" id="SKU" value={SKU} onChange={(e) => setSKU(e.target.value)} /></span>
+        </div>
 
-{typeSwitch?
-<>
-<label htmlFor="size">Size</label>
-  <input type="number" name="size" id="size" value={size} onChange={(e) => setSize(e.target.value)} />
+        <div>
+        <div htmlFor="name">Name</div>
+        <div><input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} /></div>
+       </div>
+        <div>
+        <div htmlFor="price">Price ($)</div>
+        <div><input type="number" name="price" id="price" value={price} onChange={(e) => setPrice(e.target.value)} /></div>
+        </div>
+        <div>
+        <div htmlFor="typeSwitch">Type Switcher</div>
+          <div><select name="typeSwitch" id="typeSwitch" onChange={(e) => setTypeSwitch(e.target.value)}>
+          <option value="DVD">DVD</option>
+          <option value="Furniture">Furniture</option>
+          <option value="Book">Book</option>
+          </select>
+          </div>
+        </div>
+        
 
-  <label htmlFor="height">Height</label>
-  <input type="number" name="height" id="height" value={height} onChange={(e) => setHeight(e.target.value)} />
+{typeSwitch=="DVD"?
+<div>
+<div htmlFor="size">Size (MB)</div>
+  <div><input type="number" name="size" id="size" value={size} onChange={(e) => setSize(e.target.value)} /></div>
+</div> : ""}
+{typeSwitch=="Furniture"?
+<div>
+  <div htmlFor="height">Height (CM)</div>
+  <div><input type="number" name="height" id="height" value={height} onChange={(e) => setHeight(e.target.value)} /></div>
 
-  <label htmlFor="width">Width</label>
-  <input type="number" name="width" id="width" value={width} onChange={(e) => setWidth(e.target.value)} />
+  <div htmlFor="width">Width (CM)</div>
+  <div><input type="number" name="width" id="width" value={width} onChange={(e) => setWidth(e.target.value)} /></div>
 
-  <label htmlFor="length">Length</label>
-  <input type="number" name="length" id="length" value={length} onChange={(e) => setLength(e.target.value)} />
-
-  <label htmlFor="weight">Weight</label>
-  <input type="number" name="weight" id="weight" value={weight} onChange={(e) => setWeight(e.target.value)} />
-</>
+  <div htmlFor="length">Length (CM)</div>
+  <div><input type="number" name="length" id="length" value={length} onChange={(e) => setLength(e.target.value)} /></div>
+</div> : ""}
+  
+  { typeSwitch=="Book"?
+  <div>
+  <div htmlFor="weight">Weight (KG)</div>
+  <div><input type="number" name="weight" id="weight" value={weight} onChange={(e) => setWeight(e.target.value)} /></div>
+</div>
     :
     ""
+    }
+ 
   
-}
 
-        <input type="button" name="submit" id="submit" value="SEND" onClick={handleSubmit} />
-      </div>
+      </form>
     </>
   );
 }
